bd0a00592d06ca820497cee0dcf7f1b191e3a557
shreegowri <gaganandh@gmail.com>
2022-06-08 12:44:18 +0530

    [shreegowri] FlipCoin Program

---
+++ CBasicCorePrograms/CBasicCorePrograms/FlipCoin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBasicCorePrograms
+{
+    internal class FlipCoin1
+    {
+        public void Flip_Percentage()
+        {
+            int Head = 0;
+            int Tail = 0;
+
+            Console.WriteLine("Enter the number of time you want to flip the coin");
+            int Number_of_Flips = Convert.ToInt32(Console.ReadLine());
+            while (Number_of_Flips <= 0)
+
+            {
+                Console.WriteLine("Invalid please enter the correct number times you want to filp");
+                Console.WriteLine("please enter the number again");
+                Number_of_Flips = Convert.ToInt32(Console.ReadLine());
+
+            }
+            for (int i = 0; i < Number_of_Flips; i++)
+            {
+                Random flip = new Random();
+                int HoT = flip.Next(0, 2);
+                if (HoT < 0.5)
+                {
+                    Head++;
+                }
+                else
+                {
+                    Tail++;
+                }
+                float Head_percentage = (float)Head * 100 / Number_of_Flips;
+                float Tail_percentage = (float)Tail * 100 / Number_of_Flips;
+
+                Console.WriteLine("The percentage of number of times heads" + Head_percentage);
+                Console.WriteLine("The percentage of number of times Tails" + Tail_percentage);
+            }
+
+        }
+    }
+}
---
+++ CBasicCorePrograms/CBasicCorePrograms/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CBasicCorePrograms
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the realtive number to perform some operation");
+            Console.WriteLine("Press 1 to perform the filp coin and percentage of Head and Tail");
+            int problem = Convert.ToInt32(Console.ReadLine());
+            switch (problem)
+            {
+                case 1:
+                    FlipCoin1 flipCoin = new FlipCoin1();//FlipCoin1
+                    flipCoin.Flip_Percentage();
+                    break;
+            }
+        }
+    }
