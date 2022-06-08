bb66a3c9ded6aa1bab35ae1cb96c66083ab765c0
shreegowri <gaganandh@gmail.com>
2022-06-08 12:56:55 +0530

    [shreegowri] to check the power of two

---
+++ CBasicCorePrograms/CBasicCorePrograms/PowerOf2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBasicCorePrograms
+{
+    internal class PowerOfTwo
+    {
+        public void Power_Of_Two()
+        {
+            Console.WriteLine("Enter the number to find the power");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            while (Number < 0 || Number >= 31)
+            {
+                Console.WriteLine("Enter a avalid number");
+                Number = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 1; i <= Number; i++)
+            {
+                Console.WriteLine(Math.Pow(2, i));
+            }
+        }
+    }
+}
--- CBasicCorePrograms/CBasicCorePrograms/Program.cs
+++ CBasicCorePrograms/CBasicCorePrograms/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Enter the realtive number to perform some operation");
             Console.WriteLine("Press 1 to perform the filp coin and percentage of Head and Tail");
+            Console.WriteLine("Press 2 to perform leap year");
+            Console.WriteLine("Press 3 to perform power of 2");
             int problem = Convert.ToInt32(Console.ReadLine());
             switch (problem)
             {
@@ -13,6 +15,15 @@
                     FlipCoin1 flipCoin = new FlipCoin1();//FlipCoin1
                     flipCoin.Flip_Percentage();
                     break;
+                case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.Leap_Year();
+                    break;
+                case 3:
+                    PowerTwo power = new PowerTwo();
+                    power.Power_Of_Two();
+                    break;
             }
         }
     }
+}
