c69546ab3cd90b953e96e45d84db608db1f7d3f6
shreegowri <gaganandh@gmail.com>
2022-06-08 12:48:01 +0530

    [shreegowri] it is to check leapyaer

--- CBasicCorePrograms/CBasicCorePrograms/Program.cs
+++ CBasicCorePrograms/CBasicCorePrograms/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Enter the realtive number to perform some operation");
             Console.WriteLine("Press 1 to perform the filp coin and percentage of Head and Tail");
+            Console.WriteLine("press 2 to perform leap year fuction");
             int problem = Convert.ToInt32(Console.ReadLine());
             switch (problem)
             {
@@ -13,6 +14,11 @@
                     FlipCoin1 flipCoin = new FlipCoin1();//FlipCoin1
                     flipCoin.Flip_Percentage();
                     break;
+                case 2:
+                    LeapYear leapYear = new LeapYear();
+                    leapYear.Leap_Year();
+                    break;
             }
         }
     }
+}
